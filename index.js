5d7c3562f2a60a95408f6de704f0a55a235bb110
YutoMori <b1016206@fun.ac.jp>
2018-09-03 16:23:11 +0900

    add match start SE

--- index.js
+++ index.js
@@ -5,16 +5,12 @@ const bodyParser = require('body-parser');
 const LAUNCH_MESSAGE = '数字推測、ボクシングゲーム、ヒット&ブロー、へようこそ。'
                      + 'ゲームを始めたいときは、スタート、と言ってくれ。'
                      + 'ゲームのルールがわからないときは、ルールを教えて、と言ってくれ。';
-/*
-var attributesInfo = {
-  start: 'none',
-  end: 'none'
-}
-*/
+
 const clovaSkillHandler = clova.Client
     .configureSkill()
     // スキルの起動リクエスト
     .onLaunchRequest(responseHelper => {
+      // TODO
       responseHelper.responseObject.sessionAttributes.count = 0;
       responseHelper.setSimpleSpeech({
         lang: 'ja',
@@ -31,18 +27,25 @@ const clovaSkillHandler = clova.Client
         let speech;
         switch (intent) {
           case 'StartIntent':
-            speech = {
-              lang: 'ja',
-              type: 'PlainText',
-              value: LAUNCH_MESSAGE  // TODO
-            }
-            responseHelper.setSimpleSpeech(speech)
+            const start_match_mp3 = process.env.START_MATCH_MP3;
+            responseHelper.setSpeechList([
+              {
+                type: "URL",
+                lang: "" ,
+                value: start_match_mp3
+              }, {
+                lang: 'ja',
+                type: 'PlainText',
+                value: 'スタートインテントが起動しました。',
+              }
+            ]);
             break;
 
           default:
             speech = {
               lang: 'ja',
               type: 'PlainText',
+              // TODO
               value: `${responseHelper.responseObject.sessionAttributes.count}想定しないインテントです。カスタムインテントの名前が正しいかご確認ください。`
             }
             responseHelper.setSimpleSpeech(speech)
