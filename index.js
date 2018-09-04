09c084830264c8795bc175b004bb4848659a2c97
YutoMori <b1016206@fun.ac.jp>
2018-09-04 23:09:33 +0900

    add description message

--- index.js
+++ index.js
@@ -10,9 +10,9 @@ const clovaSkillHandler = clova.Client
       responseHelper.responseObject.sessionAttributes.count = 0;
 
       const launch_mp3 = process.env.LAUNCH_MP3;
-      const LAUNCH_MESSAGE = '数字推測、ボクシングゲーム、ヒットブローに、ようこそ！'
-                     + 'ゲームを始めたいときは、スタート、と言ってくれ。'
-                     + 'ゲームのルールがわからないときは、ルールを教えて、と言ってくれ。';
+      const LAUNCH_MESSAGE = '数字推測、ボクシングゲーム、ナンバーパンチに、ようこそ！'
+                           + 'ゲームを始めたいときは、スタート、と言ってくれ。'
+                           + 'ゲームのルールがわからないときは、ルールを教えて、と言ってくれ。';
       responseHelper.setSpeechList([
         {
           type: "URL",
@@ -28,37 +28,52 @@ const clovaSkillHandler = clova.Client
 
     // カスタムインテント or ビルトインインテント
     .onIntentRequest(async responseHelper => {
-        const intent = responseHelper.getIntentName();
-        responseHelper.responseObject.sessionAttributes.count = 1;
-        // const sessionId = responseHelper.getSessionId();
-        let speech;
-        switch (intent) {
-          case 'StartIntent':
-            const start_match_mp3 = process.env.START_MATCH_MP3;
-            responseHelper.setSpeechList([
-              {
-                type: "URL",
-                lang: "" ,
-                value: start_match_mp3
-              }, {
-                lang: 'ja',
-                type: 'PlainText',
-                value: 'スタートインテントが起動しました。',
-              }
-            ]);
-            break;
+      const intent = responseHelper.getIntentName();
+      responseHelper.responseObject.sessionAttributes.count = 1;
+      // const sessionId = responseHelper.getSessionId();
+      let speech;
+      switch (intent) {
+        case DescriptionIntent:
+          const DESCRIPTION_MESSAGE = 'ナンバーパンチのルールを説明するぞ。'
+                                    + 'このゲームは3つの数字を当てるゲームだ！ '
+                                    + '君の回答に、ヒットとブローの数で答えるぞ。'
+                                    + 'ヒットは数字も位置も同じで、ブローは数字は当たっているけど位置が違うんだ。'
+                                    + '数字に重複はないから気をつけてくれ。'
+                                    + 'よくわからない場合は、検索サイトで「ヒット&ブロー ルール」で検索してくれ。'
+                                    + 'さあ、「スタート」と言ってゲームを始めよう。';
+          responseHelper.setSimpleSpeech({
+            lang: 'ja',
+            type: 'PlainText',
+            value: DESCRIPTION_MESSAGE
+          });
+          break;
 
-          default:
-            speech = {
+        case 'StartIntent':
+          const start_match_mp3 = process.env.START_MATCH_MP3;
+          responseHelper.setSpeechList([
+            {
+              type: "URL",
+              lang: "" ,
+              value: start_match_mp3
+            }, {
               lang: 'ja',
               type: 'PlainText',
-              // TODO
-              value: `${responseHelper.responseObject.sessionAttributes.count}想定しないインテントです。カスタムインテントの名前が正しいかご確認ください。`
+              value: 'スタートインテントが起動しました。',
             }
-            responseHelper.setSimpleSpeech(speech)
-            break;
+          ]);
+          break;
+
+        default:
+          speech = {
+            lang: 'ja',
+            type: 'PlainText',
+            // TODO
+            value: `${responseHelper.responseObject.sessionAttributes.count}想定しないインテントです。カスタムインテントの名前が正しいかご確認ください。`
           }
-        })
+          responseHelper.setSimpleSpeech(speech)
+          break;
+        }
+      })
 
     //終了時
     .onSessionEndedRequest(responseHelper => {
